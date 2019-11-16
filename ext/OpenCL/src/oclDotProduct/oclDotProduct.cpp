4025f5ed2cfcffd2f0fd9fa5ca73b8b9e5b463f6
David <khdavid@mail.ru>
2019-11-16 21:08:38 +0100

    refactoring

--- ext/OpenCL/src/oclDotProduct/oclDotProduct.cpp
+++ ext/OpenCL/src/oclDotProduct/oclDotProduct.cpp
@@ -30,115 +30,132 @@ namespace
       }
     }
   }
-
-
 }
+
+class DotProductCalculator
+{
+public:
+  void run()
+  {
+    // Get the NVIDIA platform
+    cl_platform_id platformId;      // OpenCL platform
+    oclGetPlatformID(&platformId);
+    std::cout << "PlatformID = " << platformId << std::endl;
+    //Get all the devices
+    cl_uint numDevices = 0;           // Number of devices available
+    std::cout << "Get the Device info and select Device..." << std::endl;
+    clGetDeviceIDs(platformId, CL_DEVICE_TYPE_GPU, 0, nullptr, &numDevices);
+    std::vector<cl_device_id> devices(numDevices);      // OpenCL device
+    clGetDeviceIDs(platformId, CL_DEVICE_TYPE_GPU, numDevices, devices.data(), nullptr);
+
+    // Get command line device options and config accordingly
+    std::cout << "# of Devices Available = " << numDevices << std::endl;
+    const cl_uint TARGET_DEVICE = 0;	        // Default Device to compute on
+    std::cout << "Using Device %u: " << TARGET_DEVICE << std::endl;
+    oclPrintDevName(LOGBOTH, devices[TARGET_DEVICE]);
+
+    reportConstant<cl_uint>(devices[TARGET_DEVICE], CL_DEVICE_MAX_COMPUTE_UNITS, "Number of compute units = ");
+    reportConstant<size_t>(devices[TARGET_DEVICE], CL_DEVICE_MAX_WORK_GROUP_SIZE, "Max Number work groups = ");
+    reportConstant<size_t>(devices[TARGET_DEVICE], CL_KERNEL_WORK_GROUP_SIZE, "Max Number kernel work groups = ");
+
+
+    // start logs
+    const int NUM_ELEMENTS = 1277944;	    // Length of float arrays to process (odd # for illustration)
+    shrLog("Starting...\n\n# of float elements per Array \t= %u\n", NUM_ELEMENTS);
+
+    // set and log Global and Local work size dimensions
+    const size_t LOCAL_WORK_SIZE = 256;
+    const auto globalWorkSize = shrRoundUp((int)LOCAL_WORK_SIZE, NUM_ELEMENTS);  // rounded up to the nearest multiple of the LocalWorkSize
+    shrLog("Global Work Size \t\t= %u\nLocal Work Size \t\t= %u\n# of Work Groups \t\t= %u\n\n",
+      globalWorkSize, LOCAL_WORK_SIZE, (globalWorkSize % LOCAL_WORK_SIZE + globalWorkSize / LOCAL_WORK_SIZE));
+
+    // Allocate and initialize host arrays
+    shrLog("Allocate and Init Host Mem...\n");
+    std::vector<cl_float4> srcA(globalWorkSize);
+    std::vector<cl_float4> srcB(globalWorkSize);
+    std::vector<cl_float> dst(globalWorkSize);
+    std::vector<cl_float> Golden(NUM_ELEMENTS);
+    shrFillArray((float*)srcA.data(), 4 * NUM_ELEMENTS);
+    shrFillArray((float*)srcB.data(), 4 * NUM_ELEMENTS);
+
+    gpuContext_ = clCreateContext(nullptr, 1, &devices[TARGET_DEVICE], nullptr, nullptr, nullptr);
+    commandQueue_ = clCreateCommandQueue(gpuContext_, devices[TARGET_DEVICE], 0, nullptr);
+
+    srcABuffer_ = clCreateBuffer(gpuContext_, CL_MEM_READ_ONLY, sizeof(cl_float4) * globalWorkSize, nullptr, nullptr);
+    srcBBuffer_ = clCreateBuffer(gpuContext_, CL_MEM_READ_ONLY, sizeof(cl_float4) * globalWorkSize, nullptr, nullptr);
+    dstBuffer_ = clCreateBuffer(gpuContext_, CL_MEM_WRITE_ONLY, sizeof(cl_float) * globalWorkSize, nullptr, nullptr);
+
+    std::cout << "Creating program" << std::endl;
+    size_t programSize = strlen(CL_PROGRAM_DOT_PRODUCT);			// Byte size of kernel code
+    gpuProgram_ = clCreateProgramWithSource(gpuContext_, 1, &CL_PROGRAM_DOT_PRODUCT, &programSize, nullptr);
+    const char* COMPILATION_FLAGS = "-cl-fast-relaxed-math";
+    std::cout << "Building program" << std::endl;
+    auto feedback = clBuildProgram(gpuProgram_, 0, nullptr, nullptr, nullptr, nullptr);
+    if (feedback != CL_SUCCESS)
+    {
+      oclLogBuildInfo(gpuProgram_, devices[TARGET_DEVICE]);
+      return;
+    }
+
+    // Create the kernel
+    std::cout << "Creating Kernel (DotProduct)..." << std::endl;
+    kernel_ = clCreateKernel(gpuProgram_, "DotProduct", &feedback);
+
+    // Set the Argument values
+    shrLog("clSetKernelArg 0 - 3...\n\n");
+    clSetKernelArg(kernel_, 0, sizeof(cl_mem), (void*)& srcABuffer_);
+    clSetKernelArg(kernel_, 1, sizeof(cl_mem), (void*)& srcBBuffer_);
+    clSetKernelArg(kernel_, 2, sizeof(cl_mem), (void*)& dstBuffer_);
+    clSetKernelArg(kernel_, 3, sizeof(cl_int), (void*)& NUM_ELEMENTS);
+
+    // --------------------------------------------------------
+    // Core sequence... copy input data to GPU, compute, copy results back
+
+    // Asynchronous write of data to GPU device
+    shrLog("clEnqueueWriteBuffer (SrcA and SrcB)...\n");
+    clEnqueueWriteBuffer(commandQueue_, srcABuffer_, CL_FALSE, 0, sizeof(cl_float) * globalWorkSize * 4, srcA.data(), 0, nullptr, nullptr);
+    clEnqueueWriteBuffer(commandQueue_, srcBBuffer_, CL_FALSE, 0, sizeof(cl_float) * globalWorkSize * 4, srcB.data(), 0, nullptr, nullptr);
+
+    // Launch kernel
+    shrLog("clEnqueueNDRangeKernel (DotProduct)...\n");
+    clEnqueueNDRangeKernel(commandQueue_, kernel_, 1, nullptr, &globalWorkSize, &LOCAL_WORK_SIZE, 0, nullptr, nullptr);
+
+    // Read back results and check accumulated errors
+    shrLog("clEnqueueReadBuffer (Dst)...\n\n");
+    clEnqueueReadBuffer(commandQueue_, dstBuffer_, CL_TRUE, 0, sizeof(cl_float) * globalWorkSize, dst.data(), 0, nullptr, nullptr);
+
+    // Compute and compare results for golden-host and report errors and pass/fail
+    shrLog("Comparing against Host/C++ computation...\n\n");
+    DotProductHost((const float*)srcA.data(), (const float*)srcB.data(), (float*)Golden.data(), NUM_ELEMENTS);
+    shrBOOL bMatch = shrComparefet((const float*)Golden.data(), (const float*)dst.data(), (unsigned int)NUM_ELEMENTS, 0.0f, 0);
+    std::cout << std::boolalpha;
+    std::cout << "COMPARING STATUS : " << bMatch << std::endl;
+
+  }
+~DotProductCalculator()
+{
+  if (srcABuffer_) clReleaseMemObject(srcABuffer_);
+  if (srcBBuffer_) clReleaseMemObject(srcBBuffer_);
+  if (dstBuffer_) clReleaseMemObject(dstBuffer_);
+  if (kernel_) clReleaseKernel(kernel_);
+  if (gpuProgram_) clReleaseProgram(gpuProgram_);
+  if (commandQueue_) clReleaseCommandQueue(commandQueue_);
+  if (gpuContext_) clReleaseContext(gpuContext_);
+}
+private:
+  cl_mem srcABuffer_ = 0;
+  cl_mem srcBBuffer_ = 0;
+  cl_mem dstBuffer_ = 0;
+  cl_kernel kernel_ = 0;
+  cl_program gpuProgram_ = 0;
+  cl_command_queue commandQueue_ = 0;
+  cl_context gpuContext_ = 0;
+};
 
 // *********************************************************************
 int main(int argc, char** argv)
 {
-  // Get the NVIDIA platform
-  cl_platform_id platformId;      // OpenCL platform
-  auto feedback = oclGetPlatformID(&platformId);
-  std::cout << "PlatformID = " << platformId << std::endl;
-  //Get all the devices
-  cl_uint numDevices = 0;           // Number of devices available
-  std::cout << "Get the Device info and select Device..." << std::endl;
-  clGetDeviceIDs(platformId, CL_DEVICE_TYPE_GPU, 0, nullptr, &numDevices);
-  std::vector<cl_device_id> devices(numDevices);      // OpenCL device
-  clGetDeviceIDs(platformId, CL_DEVICE_TYPE_GPU, numDevices, devices.data(), nullptr);
-
-  // Get command line device options and config accordingly
-  std::cout << "# of Devices Available = " << numDevices << std::endl;
-  const cl_uint TARGET_DEVICE = 0;	        // Default Device to compute on
-  std::cout << "Using Device %u: " << TARGET_DEVICE << std::endl;
-  oclPrintDevName(LOGBOTH, devices[TARGET_DEVICE]);
-
-  reportConstant<cl_uint>(devices[TARGET_DEVICE], CL_DEVICE_MAX_COMPUTE_UNITS, "Number of compute units = ");
-  reportConstant<size_t>(devices[TARGET_DEVICE], CL_DEVICE_MAX_WORK_GROUP_SIZE, "Max Number work groups = ");
-  reportConstant<size_t>(devices[TARGET_DEVICE], CL_KERNEL_WORK_GROUP_SIZE, "Max Number kernel work groups = ");
-  
-  
-  // start logs
-  const int NUM_ELEMENTS = 1277944;	    // Length of float arrays to process (odd # for illustration)
-  shrLog("Starting...\n\n# of float elements per Array \t= %u\n", NUM_ELEMENTS);
-
-  // set and log Global and Local work size dimensions
-  const size_t LOCAL_WORK_SIZE = 256;
-  const auto globalWorkSize = shrRoundUp((int)LOCAL_WORK_SIZE, NUM_ELEMENTS);  // rounded up to the nearest multiple of the LocalWorkSize
-  shrLog("Global Work Size \t\t= %u\nLocal Work Size \t\t= %u\n# of Work Groups \t\t= %u\n\n",
-    globalWorkSize, LOCAL_WORK_SIZE, (globalWorkSize % LOCAL_WORK_SIZE + globalWorkSize / LOCAL_WORK_SIZE));
-
-  // Allocate and initialize host arrays
-  shrLog("Allocate and Init Host Mem...\n");
-  std::vector<cl_float4> srcA(globalWorkSize);
-  std::vector<cl_float4> srcB(globalWorkSize);
-  std::vector<cl_float> dst(globalWorkSize);
-  std::vector<cl_float> Golden(NUM_ELEMENTS);
-  shrFillArray((float*)srcA.data(), 4 * NUM_ELEMENTS);
-  shrFillArray((float*)srcB.data(), 4 * NUM_ELEMENTS);
-
-  auto gpuContext = clCreateContext(nullptr, 1, &devices[TARGET_DEVICE], nullptr, nullptr, nullptr);
-  auto commandQueue = clCreateCommandQueue(gpuContext, devices[TARGET_DEVICE], 0, nullptr);
-
-  auto srcABuffer = clCreateBuffer(gpuContext, CL_MEM_READ_ONLY, sizeof(cl_float4) * globalWorkSize, nullptr, nullptr);
-  auto srcBBuffer = clCreateBuffer(gpuContext, CL_MEM_READ_ONLY, sizeof(cl_float4) * globalWorkSize, nullptr, nullptr);
-  auto dstBuffer = clCreateBuffer(gpuContext, CL_MEM_WRITE_ONLY, sizeof(cl_float) * globalWorkSize, nullptr, nullptr);
-
-  std::cout << "Creating program" << std::endl;
-  size_t programSize = strlen(CL_PROGRAM_DOT_PRODUCT);			// Byte size of kernel code
-  auto gpuProgram = clCreateProgramWithSource(gpuContext, 1, &CL_PROGRAM_DOT_PRODUCT, &programSize, &feedback);
-  const char* COMPILATION_FLAGS = "-cl-fast-relaxed-math";
-  std::cout << "Building program" << std::endl;
-  feedback = clBuildProgram(gpuProgram, 0, nullptr, nullptr, nullptr, nullptr);
-  if (feedback != CL_SUCCESS)
-  {
-    oclLogBuildInfo(gpuProgram, devices[TARGET_DEVICE]);
-    return 1;
-  }
-
-  // Create the kernel
-  std::cout << "Creating Kernel (DotProduct)..." << std::endl;
-  auto kernel = clCreateKernel(gpuProgram, "DotProduct", &feedback);
-
-  // Set the Argument values
-  shrLog("clSetKernelArg 0 - 3...\n\n");
-  clSetKernelArg(kernel, 0, sizeof(cl_mem), (void*)& srcABuffer);
-  clSetKernelArg(kernel, 1, sizeof(cl_mem), (void*)& srcBBuffer);
-  clSetKernelArg(kernel, 2, sizeof(cl_mem), (void*)& dstBuffer);
-  clSetKernelArg(kernel, 3, sizeof(cl_int), (void*)& NUM_ELEMENTS);
-
-  // --------------------------------------------------------
-  // Core sequence... copy input data to GPU, compute, copy results back
-
-  // Asynchronous write of data to GPU device
-  shrLog("clEnqueueWriteBuffer (SrcA and SrcB)...\n");
-  clEnqueueWriteBuffer(commandQueue, srcABuffer, CL_FALSE, 0, sizeof(cl_float) * globalWorkSize * 4, srcA.data(), 0, nullptr, nullptr);
-  clEnqueueWriteBuffer(commandQueue, srcBBuffer, CL_FALSE, 0, sizeof(cl_float) * globalWorkSize * 4, srcB.data(), 0, nullptr, nullptr);
-
-  // Launch kernel
-  shrLog("clEnqueueNDRangeKernel (DotProduct)...\n");
-  clEnqueueNDRangeKernel(commandQueue, kernel, 1, nullptr, &globalWorkSize, &LOCAL_WORK_SIZE, 0, nullptr, nullptr);
-
-  // Read back results and check accumulated errors
-  shrLog("clEnqueueReadBuffer (Dst)...\n\n");
-  clEnqueueReadBuffer(commandQueue, dstBuffer, CL_TRUE, 0, sizeof(cl_float) * globalWorkSize, dst.data(), 0, nullptr, nullptr);
-
-  // Compute and compare results for golden-host and report errors and pass/fail
-  shrLog("Comparing against Host/C++ computation...\n\n");
-  DotProductHost((const float*)srcA.data(), (const float*)srcB.data(), (float*)Golden.data(), NUM_ELEMENTS);
-  shrBOOL bMatch = shrComparefet((const float*)Golden.data(), (const float*)dst.data(), (unsigned int)NUM_ELEMENTS, 0.0f, 0);
-  std::cout << std::boolalpha;
-  std::cout << "COMPARING STATUS : " << bMatch << std::endl;
-
-  // Cleanup and leave
-  if (srcABuffer) clReleaseMemObject(srcABuffer);
-  if (srcBBuffer) clReleaseMemObject(srcBBuffer);
-  if (dstBuffer) clReleaseMemObject(dstBuffer);
-  if (kernel) clReleaseKernel(kernel);
-  if (gpuProgram) clReleaseProgram(gpuProgram);
-  if (commandQueue) clReleaseCommandQueue(commandQueue);
-  if (gpuContext) clReleaseContext(gpuContext);
-
+  DotProductCalculator dotProductCalculator;
+  dotProductCalculator.run();
   return 0;
 }
